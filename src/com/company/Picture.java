879b753166e013c57171cf3e42cb140a1420fa87
Kiara Sims <simskiara6@gmail.com>
2019-06-04 10:49:14 -0500

    Added a constructor

--- src/com/company/Picture.java
+++ src/com/company/Picture.java
@@ -3,5 +3,9 @@ package com.company;
 public class Picture {
     private String name;
     private double size;
-    
+
+    private Picture{
+        this.name = "Kiara";
+        this.size = 3.14;
+    }
 }
