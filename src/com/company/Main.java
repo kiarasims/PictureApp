a77ff85c2a912850afeeac9dc3525a50182b412a
Kiara Sims <simskiara6@gmail.com>
2019-06-06 11:52:39 -0500

    Updated object output.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("Welcome to Kiara's Picture App.");
 	Picture kiara = new Picture();
-	System.out.println("Kiara is" + kiara);
+	System.out.println("Kiara is " + kiara);
     }
 
 }
