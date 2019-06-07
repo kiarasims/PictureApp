0656bfdd31cdff2bc96b50d7b1600dfbb5b58a9c
Kiara Sims <simskiara6@gmail.com>
2019-06-06 22:58:22 -0500

    Updated the double of a size. Swap the object names.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,19 +4,32 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.println("Welcome to Kiara's Picture App.");
+
 	Picture kiara = new Picture();
 	System.out.println("Kiara is " + kiara);
+
 	Picture pic = new Picture("Lang", 1.2);
 	String name1  = pic.getName();
 	System.out.println("The object name is " + name1);
+
     pic.setName("Luna");
     System.out.println("The updated object is " + pic);
+
     double size1 = kiara.getSize();
     System.out.println("The object size is " + size1);
+
     kiara.setSize(5.23);
     System.out.println("The updated object is " + kiara);
-    double doubleSize = size1 * 2;
+
+    double objectSize = pic.getSize();
+    double doubleSize = objectSize * 2;
     System.out.println(doubleSize + " is the object size doubled.");
+
+    String objectName = kiara.getName();
+    String objectName2 = pic.getName();
+    kiara.setName(objectName2);
+    pic.setName(objectName);
+    System.out.println(kiara + " and " + pic);
     }
 
 }
